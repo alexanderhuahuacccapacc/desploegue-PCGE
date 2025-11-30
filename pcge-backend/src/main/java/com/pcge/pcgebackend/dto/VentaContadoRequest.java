8effbda02bdbea58f577ff3e3346bd4146227a96
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-11-30 16:41:15 -0500

    cambio-mantenimiento_1

--- pcge-backend/src/main/java/com/pcge/pcgebackend/dto/VentaContadoRequest.java
+++ pcge-backend/src/main/java/com/pcge/pcgebackend/dto/VentaContadoRequest.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 @Data
 public class VentaContadoRequest {
+    private Long numeroOperacion;
     private String cliente;
     private BigDecimal montoTotal;
     private String descripcion;
@@ -14,6 +15,26 @@ public class VentaContadoRequest {
     private String numeroDocumentoIdentidad;
     private String fechaEmision;
     private String fechaVencimiento;
+
+    public VentaContadoRequest() {}
+
+    public VentaContadoRequest(Long numeroOperacion, String cliente, BigDecimal montoTotal,
+                               String descripcion, String tipoComprobante, String numeroSerie,
+                               String numeroDocumento, String tipoDocumentoIdentidad,
+                               String numeroDocumentoIdentidad, String fechaEmision,
+                               String fechaVencimiento) {
+        this.numeroOperacion = numeroOperacion;
+        this.cliente = cliente;
+        this.montoTotal = montoTotal;
+        this.descripcion = descripcion;
+        this.tipoComprobante = tipoComprobante;
+        this.numeroSerie = numeroSerie;
+        this.numeroDocumento = numeroDocumento;
+        this.tipoDocumentoIdentidad = tipoDocumentoIdentidad;
+        this.numeroDocumentoIdentidad = numeroDocumentoIdentidad;
+        this.fechaEmision = fechaEmision;
+        this.fechaVencimiento = fechaVencimiento;
+    }
 
 
 }
--- pcge-backend/src/main/java/com/pcge/pcgebackend/dto/VentaCreditoRequest.java
+++ pcge-backend/src/main/java/com/pcge/pcgebackend/dto/VentaCreditoRequest.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.math.BigDecimal;
 @Data
 public class VentaCreditoRequest {
+    private Long numeroOperacion;
     private String cliente;
     private BigDecimal montoTotal;
     private String descripcion;
@@ -19,10 +20,11 @@ public class VentaCreditoRequest {
     // Constructores
     public VentaCreditoRequest() {}
 
-    public VentaCreditoRequest(String cliente, BigDecimal montoTotal, String descripcion,
+    public VentaCreditoRequest(Long numeroOperacion, String cliente, BigDecimal montoTotal, String descripcion,
                                String tipoComprobante, String numeroSerie, String numeroDocumento,
                                String tipoDocumentoIdentidad, String numeroDocumentoIdentidad,
                                String fechaEmision, String fechaVencimiento) {
+        this.numeroOperacion = numeroOperacion;
         this.cliente = cliente;
         this.montoTotal = montoTotal;
         this.descripcion = descripcion;
